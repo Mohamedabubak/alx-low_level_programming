cd4e561bb01141c87aa8588e757df2fa8d0adf7c
Mohamedabubak <abubakermohamed903@gmail.com>
2023-09-07 19:12:28 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-    main (void) {
-    puts ("Programming is like building a multilingual puzzle\n");
-    return 0 ;
-    }
+/**
+ * main entry point 
+ * Description: A C program that prints with put function
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	pust("\"Programming is like building a multilingual puzzle");
+	return (0);
+}    
